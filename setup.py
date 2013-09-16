3c73035dc48e624339a1a6b9436cf2a33bdf3408
Markus Holtermann <info@markusholtermann.eu>
2013-09-16 22:50:32 +0200

    Bump next version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
