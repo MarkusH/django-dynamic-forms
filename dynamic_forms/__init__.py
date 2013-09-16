b0be0cac3c47963745b746fb533853a21c282248
Markus Holtermann <info@markusholtermann.eu>
2013-09-16 21:04:16 +0200

    Release 0.1

--- dynamic_forms/__init__.py
+++ dynamic_forms/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 
-VERSION = (0, 1, 0, 'alpha', 0)
+VERSION = (0, 1, 0, 'final', 0)
 
 
 def get_version(full=True):
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
