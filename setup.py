855e639f2024e979d6725535a40e0cba1c515701
Markus Holtermann <info@markusholtermann.eu>
2013-09-18 00:15:21 +0200

    Fix setup.py and related stuff. Thanks jezdez

--- setup.py
+++ setup.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+import os
+import codecs
+from setuptools import setup
 
 
-with open('README.rst') as f:
-    readme = f.read()
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
 
-with open('requirements.txt') as f:
-    requirements = [l.strip() for l in f.readlines()]
 
 from dynamic_forms import get_version
+
 
 setup(
     name='django-dynamic-forms',
     version=get_version(),
     description='django-dynamic-forms is a reusable Django application to create and configure forms through the admin.',
-    long_description=readme,
+    long_description=read('README.rst'),
     author='Markus Holtermann',
     author_email='info@markusholtermann.eu',
     url='http://github.com/Markush2010/django-dynamic-forms',
     license='BSD',
     packages=[
         'dynamic_forms',
-        'example',
-        'example.example',
-        'tests',
+        'dynamic_forms.migrations',
     ],
     package_data = {
         'dynamic_forms': [
             'locale/*/LC_MESSAGES/*',
             'templates/dynamic_forms/*',
-        ],
-        'example': [
-            'example.db',
         ]
     },
-    install_requires=requirements,
+    install_requires=[
+        'Django>=1.4',
+        'django-appconf>=0.6',
+        'six',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -45,9 +47,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Framework :: Django',
     ],
-    include_package_data=True,
     zip_safe=False
 )
