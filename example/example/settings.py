51542f13250b40a86ee31e3f9789b17fedb7ad53
gladson <gladson@immensa.com.br>
2013-12-11 18:16:48 -0400

    add path dynamic_forms in example manage.py and add language pt_BR

--- example/example/settings.py
+++ example/example/settings.py
@@ -24,7 +24,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 SITE_ID = 1
 
