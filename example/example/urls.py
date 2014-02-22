6f201073b6e43b9a4ceeb73d1b8821bd313d25aa
Markus Holtermann <info@markusholtermann.eu>
2014-02-22 20:27:17 +0100

    Add i18n to example project

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,11 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, patterns, url
+from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
     url(r'^dynamic_forms/',
         include('dynamic_forms.urls', namespace='dynamic_forms')),
-    # url(r'^captcha/', include('captcha.urls')),
 )
+
+# Uncomment for captcha fields
+# urlpatterns += patterns('',
+#     url(r'^captcha/', include('captcha.urls')),
+# )
