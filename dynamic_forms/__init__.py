c79dd41ed5ac58a93bf038443cafe8f0ca9d28d2
Markus Holtermann <info@markusholtermann.eu>
2014-09-17 16:38:17 +0200

    Bump version: 0.3.1 → 0.3.2

--- dynamic_forms/__init__.py
+++ dynamic_forms/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 
 __author__ = 'Markus Holtermann'
 __email__ = 'info@sinnwerkstatt.com'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 default_app_config = 'dynamic_forms.apps.DynamicFormsConfig'
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setup(
     name='django-dynamic-forms',
-    version='0.3.1',
+    version='0.3.2',
     description='django-dynamic-forms is a reusable Django application to create and configure forms through the admin.',
     long_description=read('README.rst'),
     author='Markus Holtermann',
