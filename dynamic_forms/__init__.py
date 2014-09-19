fc5e34aca23d219dd55ee4cfa0776ac47a4252db
Markus Holtermann <info@markusholtermann.eu>
2014-09-19 09:19:42 +0200

    Fix email in package init

--- dynamic_forms/__init__.py
+++ dynamic_forms/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Markus Holtermann'
-__email__ = 'info@sinnwerkstatt.com'
+__email__ = 'info@markusholtermann.eu'
 __version__ = '0.3.2'
 
 default_app_config = 'dynamic_forms.apps.DynamicFormsConfig'
