a35a47ce46c83b3b5a91c851f9338d2e0d937f92
Markus Holtermann <info@markusholtermann.eu>
2016-05-21 11:42:48 +0200

    Bumped release version after release branch

--- dynamic_forms/__init__.py
+++ dynamic_forms/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 
 __author__ = 'Markus Holtermann'
 __email__ = 'info@markusholtermann.eu'
-__version__ = '0.5.0'
+__version__ = '0.6.0.dev0'
 
 default_app_config = 'dynamic_forms.apps.DynamicFormsConfig'
